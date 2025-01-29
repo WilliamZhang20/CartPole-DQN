7177564059e862456db0b2a904552f94c43f6bc1
William Zhang <wzhang20@yahoo.com>
2025-01-28 22:36:26 -0500

    finalize revamping

--- agent.py
+++ agent.py
@@ -1,6 +1,11 @@
 import numpy as np
 import tensorflow as tf
+import gymnasium as gym
+import time
+import memory
+import math
 
+import matplotlib.pyplot as plt
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.losses import MSE
@@ -8,12 +13,17 @@ from tensorflow.keras.optimizers import Adam
 
 MODEL_CACHE = 'Cartpole_DQN_Model.keras'
 
-class Agent:
+class Cartpole_RL_Agent:
     def __init__(self, state_size, action_size):
         self.alpha = 0.001 # learning rate
-        self.memory_size = 10000 # replay memory
         self.gamma = 0.999 # discount factor between episodes (from MDPs)
         self.tau = 0.005 # weight of q-network in soft updates (very small for stability)
+
+        # plot array
+        self.total_point_history = []
+        
+        # Replay memory storage
+        self.memory_buffer = memory.ReplayMemory(10000)
 
         # Q network helps approx a function from state and action to a goodness value
         self.q_network = Sequential([
@@ -50,7 +60,7 @@ class Agent:
         return loss
     
     @tf.function # turn on computation graph construction!
-    def agent_learn(self, experiences):
+    def _agent_learn(self, experiences):
         with tf.GradientTape() as tape:
             loss = self._compute_loss(experiences)
         
@@ -70,4 +80,95 @@ class Agent:
         ):
             target_weights.assign(self.tau * q_net_weights + (1.0 - self.tau) * target_weights)
 
+    def get_action(self, q_values, epsilon=0.0):
+        # use ε-greedy policy
+        if memory.random.random() > epsilon:
+            return np.argmax(q_values.numpy()[0])
+        else:
+            return memory.random.choice(np.arange(4))
+
+    def _sample_memory(self, batch_size):
+        transitions = self.memory_buffer.sample(batch_size)
+        states = tf.convert_to_tensor(
+            np.array([e.state for e in transitions if e is not None]), dtype=tf.float32
+        )
+        actions = tf.convert_to_tensor(
+            np.array([e.action for e in transitions if e is not None]), dtype=tf.float32
+        )
+        rewards = tf.convert_to_tensor(
+            np.array([e.reward for e in transitions if e is not None]), dtype=tf.float32
+        )
+        next_states = tf.convert_to_tensor(
+            np.array([e.next_state for e in transitions if e is not None]), dtype=tf.float32
+        )
+        done_vals = tf.convert_to_tensor(
+            np.array([e.done for e in transitions if e is not None]), dtype=tf.float32
+        )
+        return (states, actions, next_states, rewards, done_vals)
+
     def train_episodes(self, num_episodes):
+        start = time.time()
+        self.total_point_history = []
+        env = gym.make('CartPole-v1')
+
+        episode_rec = 100
+        max_iters = 1000
+        batch_size = 64
+        e_decay = 0.99
+        e_min = 0.01
+        epsilon = 1.0
+
+        self.memory_buffer.clear()
+
+        for i in range(num_episodes):
+            curr_state, _ = env.reset()
+            total_points = 0
+            for t in range(max_iters):
+                # transform into q_network input
+                state_qn = np.expand_dims(curr_state, axis=0)
+                q_values = self.q_network(state_qn)
+
+                # get action by random choice between explore vs exploit
+                action = self.get_action(q_values, epsilon)
+
+                # take next action - retrieve environ reward
+                next_state, reward, done, _ , _ = env.step(action)
+
+                # record in memory
+                self.memory_buffer.append(memory.Transition(curr_state, action, next_state, reward, done))
+
+                # complete soft update every episode...
+                experiences = self._sample_memory(batch_size)
+
+                self._agent_learn(experiences)
+
+                curr_state = next_state.copy() # copy over numpy array
+                total_points += reward    
+
+                if done:
+                    break
+            
+            self.total_point_history.append(total_points)
+            epsilon = max(e_min, e_decay * epsilon)
+            avg_latest_points=np.mean(self.total_point_history[-episode_rec:])
+            
+            if (i+1) % episode_rec == 0:
+                print(f"\rEpisode {i+1} | Total point average of the last {episode_rec} episodes: {avg_latest_points:.2f}")
+
+        time_taken = time.time() - start
+        print(f"\nTotal Runtime: {time_taken:.2f} s ({(time_taken/60):.2f} min)")
+        self.q_network.save(MODEL_CACHE)
+
+    def plot_history(self):
+        # from the element `total point history`
+        plt.plot(range(len(self.total_point_history)), self.total_point_history)
+        plt.xlabel('Episode')
+        plt.ylabel('Total Reward')
+        plt.title('Learning Progress Over Time')
+        plt.savefig('learning_progress.png')  # Save the plot as an image file
+        plt.show()  # Display the plot
+
+    @staticmethod
+    def load_model(self):
+        self.q_network = tf.keras.models.load_model(MODEL_CACHE)
+        self.target_network = self.q_network
--- train_cartpole.py
+++ train_cartpole.py
@@ -1,100 +1,40 @@
-from agent import Agent
+from agent import Cartpole_RL_Agent
 import gymnasium as gym
-from gymnasium.wrappers import RecordVideo
 import numpy as np
-import matplotlib.pyplot as plt 
-
-def train_agent():
-
-    env = gym.make("CartPole-v1")
-    state_size = env.observation_space.shape
-    action_size = env.action_space.n # only 2 actions left or right!
-
-    print('State Shape:', state_size) # (4, )
-    print('Number of actions:', action_size)
-
-    n_episodes = 300 # set to 300 episodes
-    max_iteration_ep = 500
-
-    # Agent defined
-    agent = Agent(state_size, action_size)
-    total_steps = 0
-    episode_rewards = []  # List to store total rewards per episode
-
-    for e in range(n_episodes):
-        current_state, _ = env.reset() # env.reset() returns a tuple, discard second
-        
-        current_state = np.array(current_state, dtype=np.float32)
-        current_state = np.reshape(current_state, [1, state_size])
-        print("Episode:", e)
-
-        total_rewards = 0  # Track total rewards for the episode
-
-        for step in range(max_iteration_ep):
-            total_steps = total_steps + 1
-            # Agent computes actions in training setting
-            action = agent.act(current_state)
-            # Run the action
-            next_state, reward, done, _, _ = env.step(action) # length of step tuple result is 5
-
-            next_state = np.array([next_state])
-
-            total_rewards += reward
-
-            agent.store_episode(current_state, action, reward, next_state, done)
-
-            if done:
-                agent.update_exp_prob()
-                break
-
-            current_state = next_state
-
-        episode_rewards.append(total_rewards)  # Store the total rewards for the episode
-
-        if total_steps >= agent.batch_size:
-            agent.train()
-
-    # plot data of the "learning curve"
-    # Plot the graph at the end of training
-    plt.plot(range(len(episode_rewards)), episode_rewards)
-    plt.xlabel('Episode')
-    plt.ylabel('Total Reward')
-    plt.title('Learning Progress Over Time')
-    plt.savefig('learning_progress.png')  # Save the plot as an image file
-    plt.show()  # Display the plot
-
-    print("Done training")
-    agent.save('trained_agent.pkl')
-    return agent
+from gymnasium.wrappers import RecordVideo
 
 def make_video(agent):
     env = gym.make('CartPole-v1', render_mode="rgb_array")
+
+    # RecordVideo is convenient cause it will simply auto-save the video!
     env = RecordVideo(env, video_folder="cartpole-video", name_prefix="eval",
                   episode_trigger=lambda x: True)
-
-    rewards = 0
-    steps = 0
+    
     done = False
-    state, _ = env.reset() # split out the tuple
-    state = np.array([state])
-    agent.exp_prob = 0 # block out all explorations
+    state, _ = env.reset() # split out the tuple, trash second info
 
     while not done:
-        action = agent.act(state)
-        state, reward, done, _, _ = env.step(action)
-        state = np.array([state])
-        steps += 1
-        rewards += reward
+        state_qn = np.expand_dims(state, axis=0)
+        q_values = agent.q_network(state_qn)
+        action = agent.get_action(q_values)
+        state, _, done, _, _ = env.step(action)
 
-    print(rewards)
+    agent.plot_history() # show a plot after training!
     env.close()
+
+env = gym.make("CartPole-v1")
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n # only 2 actions left or right!
+
+# Number of training episodes
+num_episodes = 600
+
+# Define the agent
+agent = Cartpole_RL_Agent(state_size, action_size)
 
 option = input()
 if option == "1":
-    agent = train_agent()
+    agent.train_episodes(num_episodes)
 else:
-    env = gym.make("CartPole-v1")
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n # only 2 actions left or right!
-    agent = Agent.load('trained_agent.pkl')
+    agent = Cartpole_RL_Agent.load_model()
 make_video(agent)
